78c60aa5dc49a2765a243e86d7190f81e8c3529d
BaldiMaulana <m.baldimaulana.com>
2024-08-06 00:37:25 +0700

    fix

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -4,9 +4,9 @@ const Headline = () => {
     return (
         <div className="grid justify-center items-center text-lg space-y-2">
             <div className="text-center">
-            <h1 className="uppercase text-redto text-4xl font-extrabold text-center"> alat cukur elektrik anti luka & iritasi  </h1>
-            <h2 className="capitalize text-xl font-bold">diskon 50% + diskon ongkir + 
-                <span className="text-redto"> GRATIS E-BOOK</span> khusus hari ini saja! </h2>
+                <h1 className="uppercase text-redto text-4xl font-extrabold text-center"> alat cukur elektrik anti luka & iritasi  </h1>
+                <h2 className="capitalize text-xl font-bold">diskon 50% + diskon ongkir +
+                    <span className="text-redto"> GRATIS E-BOOK</span> khusus hari ini saja! </h2>
             </div>
             <div className="flex justify-center items-center">
                 <img src="/images/header.webp" alt="produk" />
@@ -20,17 +20,17 @@ const Headline = () => {
                 <h1 className="text-center capitalize text-3xl ">KEUNGGULAN PRODUK</h1>
             </div>
 
-                <div className="grid justify-start items-center text-justify">
-                    <p>✅ Cukup 1 alat untuk segala kebutuhan</p>
-                    <p>✅ Stabil dan tidak bising</p>
-                    <p>✅ Gak perlu ganti-ganti baterai, tinggal di charge gak ribet</p>
-                    <p>✅ Mudah dibersihan, sehingga mencukur tetap higenis tanpa khawatir iritasi</p>
-                    <p>✅ Awet, kuat dan anti karat</p>
-                </div>
+            <ul className="grid justify-start items-center text-justify">
+                <li>✅ Cukup 1 alat untuk segala kebutuhan</li>
+                <li>✅ Stabil dan tidak bising</li>
+                <li>✅ Gak perlu ganti-ganti baterai, tinggal di charge gak ribet</li>
+                <li>✅ Mudah dibersihan, sehingga mencukur tetap higenis tanpa khawatir iritasi</li>
+                <li>✅ Awet, kuat dan anti karat</li>
+            </ul>
 
             <div className="space-y-2 grid justify-center">
-                <img src="/images/spek.webp" alt="produk"/>
-                <img src="/images/spek2.webp" alt="produk"/>
+                <img src="/images/spek.webp" alt="produk" />
+                <img src="/images/spek2.webp" alt="produk" />
             </div>
 
             <div>
@@ -41,7 +41,7 @@ const Headline = () => {
                     <li>Power : 3W</li>
                     <li>Daya : Baterry charging</li>
                     <li>Bahan : ABS + d proses pelapisan listrik</li>
-                        <br/>
+                    <br />
                     <li>Isi Kemasan:</li>
                     <li>1 * Mini Shaver V3</li>
                     <li>1 * Sikat pembersih</li>
@@ -50,7 +50,7 @@ const Headline = () => {
                 </ul>
             </div>
 
-<br/>
+            <br />
 
             <div className="space-y-2">
                 <h1 className="text-center text-3xl font-bold">Berapa Harganya ?</h1>
