38e6cda204758920890872e25e495b77ca89d211
BaldiMaulana <m.baldimaulana.com>
2024-08-07 00:04:55 +0700

    fix

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -11,14 +11,14 @@ const MainContent = () => {
 
         <div>
             <Headline/>
-            <div>
-            <img src='/images/diskonspesial.webp' alt='promo' className="h-auto w-[100%]"/>
+            <div className='grid justify-center items-center'>
+            <img src='/images/diskonspesial.webp' alt='promo' className="h-auto w-25"/>
             <p className='text-3xl text-center font-extrabold'>Promo Diskon Spesial akan berakhir dalam : </p>
             <CountdownTimer hours={0} minutes={9} seconds={0}/>
             </div>
             <br/>
-            <div className='m-4'>
-                <img src='/images/2.webp' alt='cod' className="h-auto w-[100%]"/>
+            <div className='m-4 flex justify-center items-center'>
+                <img src='/images/2.webp' alt='cod' className="h-auto w-25"/>
             </div>
             <div className='text-center space-x-6 text-redto text-5xl animate-bounce'>
                 <FontAwesomeIcon icon={ faArrowDown}/>
@@ -28,14 +28,14 @@ const MainContent = () => {
             <Form />
 
             <div className='space-y-2'>
-                <img src='/images/testi (1).webp' alt='testi1' className="h-auto w-[100%]"/>
-                <img src='/images/testi (2).webp' alt='testi2' className="h-auto w-[100%]"/>
-                <img src='/images/testi (3).webp' alt='testi3' className="h-auto w-[100%]"/>
+                <img src='/images/testi (1).webp' alt='testi1' className="h-auto w-25"/>
+                <img src='/images/testi (2).webp' alt='testi2' className="h-auto w-25"/>
+                <img src='/images/testi (3).webp' alt='testi3' className="h-auto w-25"/>
             </div>
 
             <div className='grid justify-center'>
-                <img src='/images/fotter.webp' alt='footer' className="h-auto w-[100%]"/>
-                <img src='/images/fotter2.webp' alt='footer' className="h-auto w-[100%]"/>
+                <img src='/images/fotter.webp' alt='footer' className="h-auto w-25"/>
+                <img src='/images/fotter2.webp' alt='footer' className="h-auto w-25"/>
             </div>
         </div>
     );
