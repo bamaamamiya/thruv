eb73e7c7f5b99a4c8a53681d78bc4edc82c8e596
BaldiMaulana <m.baldimaulana.com>
2024-08-13 10:22:45 +0700

    fix

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,32 +10,31 @@ const MainContent = () => {
     return (
 
         <div>
-            <Headline/>
+            <Headline />
             <div className='grid justify-center items-center'>
-            <img src='/images/diskonspesial.webp' alt='promo' width="640" height="360"/>
-            <p className='text-3xl text-center font-extrabold'>Promo Diskon Spesial akan berakhir dalam : </p>
-            <CountdownTimer hours={0} minutes={9} seconds={0}/>
+                <img src='/images/diskonspesial.webp' alt='promo' width="640" height="360" />
+                <p className='text-3xl text-center font-extrabold'>Promo Diskon Spesial akan berakhir dalam : </p>
+                <CountdownTimer hours={0} minutes={9} seconds={0} />
+                <br />
+                    <img src='/images/2.webp' alt='cod' width="640" height="360" />
             </div>
-            <br/>
-            <div className='m-4 flex justify-center items-center'>
-                <img src='/images/2.webp' alt='cod' width="640" height="360"/>
-            </div>
+
             <div className='text-center space-x-6 text-redto text-5xl animate-bounce'>
-                <FontAwesomeIcon icon={ faArrowDown}/>
-                <FontAwesomeIcon icon={ faArrowDown}/>
-                <FontAwesomeIcon icon={ faArrowDown}/>
+                <FontAwesomeIcon icon={faArrowDown} />
+                <FontAwesomeIcon icon={faArrowDown} />
+                <FontAwesomeIcon icon={faArrowDown} />
             </div>
             <Form />
 
             <div className='space-y-2 grid justify-center'>
-                <img src='/images/testi (1).webp' alt='testi1' width="640" height="360"/>
-                <img src='/images/testi (2).webp' alt='testi2' width="640" height="360"/>
-                <img src='/images/testi (3).webp' alt='testi3' width="640" height="360"/>
+                <img src='/images/testi (1).webp' alt='testi1' width="640" height="360" />
+                <img src='/images/testi (2).webp' alt='testi2' width="640" height="360" />
+                <img src='/images/testi (3).webp' alt='testi3' width="640" height="360" />
             </div>
 
             <div className='grid justify-center'>
-                <img src='/images/fotter.webp' alt='footer' width="640" height="360"/>
-                <img src='/images/fotter2.webp' alt='footer' width="640" height="360"/>
+                <img src='/images/fotter.webp' alt='footer' width="640" height="360" />
+                <img src='/images/fotter2.webp' alt='footer' width="640" height="360" />
             </div>
         </div>
     );
