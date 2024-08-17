5aa2d4e67b8c8c6e9b0591997d1aaad15a166478
BaldiMaulana <m.baldimaulana.com>
2024-08-17 14:43:35 +0700

    fix

--- src/components/product/Pel.jsx
+++ src/components/product/Pel.jsx
@@ -90,7 +90,7 @@ const Pel = () => {
                     <Form />
                 </div>
 
-                <div>
+                <div className="grid justify-center items-center">
                     <img src="images/fotter2.webp" alt="footer" width="640" height="360" />
                     <img src="images/fotter.webp" alt="footer" width="640" height="360" />
                 </div>
