353f8dce6b94d5e6d5560cf8344c436787f29ebe
BaldiMaulana <m.baldimaulana.com>
2024-08-26 16:05:53 +0700

    fix

--- src/components/product/Pel.jsx
+++ src/components/product/Pel.jsx
@@ -3,6 +3,8 @@ import CountdownTimer from "../set/CountdownTimer";
 import Garansi from "../set/Garansi";
 import Form from './Form'
 
+import Floting from "./Floting";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
 
@@ -46,7 +48,7 @@ const Pel = () => {
 
             <div className="grid justify-center items-center text-center">
                 <h1 className="font-bold text-2xl m-2">Diskon <span className="text-redto font-bold">50%</span> Khusus Untuk Hari Ini</h1>
-                <br/>
+                <br />
                 <p className="font-bold text-2xl">Promo Akan Berakhir Dalam : </p>
                 <br />
                 <CountdownTimer hours={0} minutes={9} seconds={0} />
@@ -59,13 +61,22 @@ const Pel = () => {
                 <div className="grid justify-center items-center">
                     <br />
                     <img src="images/pel/tangansakit.webp" alt="tanganpain" width="640" height="360" />
-                    <p className="uppercase text-center font-bold text-3xl">tangan pegal & sakit karena memeras kain pel ? </p>
+                    <br />
+                    <br />
+                    <p className="uppercase text-center font-bold text-3xl m-2">tangan pegal & sakit karena memeras kain pel ? </p>
                 </div>
+                <br />
+                <br />
+                <br />
                 <div className="grid justify-center items-center text-center">
                     <br />
-                    <p className="uppercase text-center font-bold text-3xl">KALAU IYA, ANDA TIDAK PERLU KHAWATIR LAGI!</p>
+                    <p className="uppercase text-center font-bold text-3xl m-2">KALAU IYA, ANDA TIDAK PERLU KHAWATIR LAGI!</p>
                     <img src="images/pel/peraspel.webp" alt="peraspel" width="640" height="360" />
+                    <br />
+                    <br />
                     <p>Karena dengan "Pel Anti Pegal", Anda tidak perlu lagi capek & pegal memeras kain pel di hidup Anda lagi.</p>
+                    <br />
+                    <br />
                 </div>
                 <div className="flex justify-center items-center">
                     <img src="images/pel/peras.webp" alt="peras" width="640" height="360" />
@@ -74,14 +85,20 @@ const Pel = () => {
                 <div className="text-xl space-y-2">
                     <br />
                     <div className="text-center">
+                        <br />
+                        <br />
+                        <br />
                         <p className="font-bold text-center">Apa itu "Pel Karet Anti Pegal " ?</p>
                         <p>"Pel Anti Pegal" adalah pel yang bisa memeras kainnya sendiri.</p>
                         <p>Menyerap air sangat kuat dalam beberapa detik saja,yang membuat anda tidak perlu khawatir kalau ada yang basah</p>
+                        <br />
+                        <br />
+                        <br />
                     </div>
                     <br />
                     <br />
-                    <p className="font-bold text-center">Kenapa Anda Harus Punya "Pel anti Pegal" ini ?</p>
-                    <ul className="list-inside list-disc grid  justify-center items-center">
+                    <p className="font-bold text-center text-2xl m-2">Kenapa Anda Harus Punya "Pel anti Pegal" ini ?</p>
+                    <ul className="list-inside list-disc grid  justify-center items-center m-2">
                         <li>Tidak perlu pegal lagi memeras kain pel.</li>
                         <li>Karet pel halus dan tahan lama</li>
                         <li>Gagang pel stainless steel (anti karat)</li>
@@ -89,6 +106,7 @@ const Pel = () => {
                         <li>Tinggi pel 96 -125 cm</li>
                         <li>Daya serap spons tinggi</li>
                     </ul>
+                    <br />
                 </div>
                 <br />
                 <br />
@@ -120,25 +138,31 @@ const Pel = () => {
                     <div className="p-4 bg-yellto text-center">
                         <p className="font-bold">Promo Akan Berakhir Dalam : </p>
                         <CountdownTimer hours={0} minutes={9} seconds={0} />
+                        
                     </div>
-
-                    <h1 className="text-center font-bold p-2 text-2xl">Isi Formulir untuk melakukan pemesanan</h1>
-                    <div className='text-center space-x-6 text-redto text-5xl animate-bounce'>
+                    <br />
+                    <br />
+                    <h1 className="text-center font-bold p-2 text-2xl" id="formulir">Isi Formulir untuk melakukan pemesanan</h1>
+                    <br />
+                    <br />
+                    <div className='text-center space-x-6 text-redto text-5xl animate-bounce' >
                         <FontAwesomeIcon icon={faArrowDown} />
                         <FontAwesomeIcon icon={faArrowDown} />
                         <FontAwesomeIcon icon={faArrowDown} />
                     </div>
                     <Form />
                 </div>
-
+                
+                <br />
+                <br />
                 <div className="grid justify-center items-center">
                     <img src="images/fotter2.webp" alt="footer" width="640" height="360" />
                     <img src="images/fotter.webp" alt="footer" width="640" height="360" />
                 </div>
+                <br />
             </div>
 
-
-
+            <Floting />
             {/* end div */}
         </div>
 
