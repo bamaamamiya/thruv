69b9ac054638812de2a1897b3c342164a9326012
BaldiMaulana <m.baldimaulana.com>
2024-09-01 05:47:26 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import MainContent from './components/MainContent';
 import Pel from './components/product/Pel'
 
 import './index.css'
+import Page from './components/Page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<MainContent />} />
+          <Route path='/' element={<Page/>}/>
+          <Route path='/shaver' element={<MainContent />} />
           <Route path='/pel' element={<Pel/>}/>
       </Routes>
     </BrowserRouter>
