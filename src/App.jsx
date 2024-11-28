efb2f3264df9df92352d423f2c357624087a2736
Bama <bamaamamiya@gmail.com>
2024-11-28 10:01:49 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainContent from './components/MainContent';
-import Pel from './components/product/Pel'
 
 import './index.css'
 import Page from './components/Page';
+import Pel from './components/Pel/Pel'
 import Flat from './components/Pel Flat/Flat';
 import Flatl from './components/Pel Flat L/Flat'
 
