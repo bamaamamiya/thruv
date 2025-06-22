a51c1472c5cba3566f6d9438db07a3d8ae12c2d8
Bama <bamaamamiya@gmail.com>
2025-06-22 17:23:06 +0700

    update

--- src/components/Apar/LandingPage.jsx
+++ src/components/Apar/LandingPage.jsx
@@ -79,7 +79,7 @@ const LandingPage = () => {
         testimonies={testimonies}
       />
       <Footer
-        pixelId="2111198546014232"
+        pixelId="1250623926188998"
         produkBaru={funnelProduct}
         footerImages={["images/fotter2.webp", "images/fotter.webp"]}
         faqs={faqs}
