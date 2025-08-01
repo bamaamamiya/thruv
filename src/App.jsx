3335138b3a6c2fa7f485a94e895e90918274c88d
Bama <bamaamamiya@gmail.com>
2025-08-01 14:42:09 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import Page from "./components/Page";
-import Pel from "./components/Pel/Pel";
-import Flat from "./components/Pel Flat/Flat";
-import PelX from "./components/Pel X/X";
 import LandingPage from "./components/LandingPage/LandingPage";
 import LandingPagePump from "./components/Pump/LandingPage";
 import LandingPageApar from "./components/Apar/LandingPage";
 import LeadsDashboard from "./components/dashboard/LeadsDashboard";
 
 import LoginPage from "./components/dashboard/LoginPage";
-import Dashboard from "./components/dashboard/DashBoard";
 import PrivateRoute from "./components/dashboard/PrivateRoute";
 import CctvBohlam from "./components/Cctv/LandingPage";
-
+import AlatPijat from './components/Pijat/LandingPage'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Page />} />
-        <Route path="/pel" element={<Pel />} />
-        <Route path="/flat" element={<Flat />} />
-        <Route path="/pelx" element={<PelX />} />
         <Route path="/lp" element={<LandingPage />} />
         <Route path="/pump" element={<LandingPagePump />} />
         <Route path="/apar" element={<LandingPageApar />} />
         <Route path="/cctv" element={<CctvBohlam />} />
+        <Route path="/pijat" element={<AlatPijat />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/dashboard"
---
+++ src/components/Pijat/Footer.jsx
@@ -0,0 +1,190 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import CountdownTimer from "../set/CountdownTimer";
+import Funnel from "../FunnelPurchase";
+import Faqs from "../set/Faqs";
+import { useEffect, useRef, useState } from "react";
+import PriceDisplay from "../set/PriceDisplay";
+import BonusTF from "../set/BonusTF";
+import PaymentComparison from "../set/PaymentComparisonTable";
+const Footer = ({
+  pixelId,
+  produkBaru,
+  footerImages,
+  faqs,
+  namaProduct,
+  bundles,
+  hargaJual,
+}) => {
+  const normalPrice = 249000; // harga normal
+  const promoPrice = hargaJual; // harga promo
+  const [isVisible, setIsVisible] = useState(false);
+  const [currentValue, setCurrentValue] = useState(normalPrice);
+  const promoRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (promoRef.current) {
+      observer.observe(promoRef.current);
+    }
+
+    return () => {
+      if (promoRef.current) {
+        observer.unobserve(promoRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible && currentValue > promoPrice) {
+      const step = (normalPrice - promoPrice) / 50; // adjust speed
+      const interval = setInterval(() => {
+        setCurrentValue((prev) => {
+          if (prev - step <= promoPrice) {
+            clearInterval(interval);
+            return promoPrice;
+          }
+          return prev - step;
+        });
+      }, 30);
+    }
+  }, [isVisible]);
+  return (
+    <div>
+      <div className="space-y-2">
+        <h1 className="text-2xl font-bold text-center" id="formulir">
+          ⚠ Promo 50% Hanya Untuk 3 Orang Pemesanan Pertama ⚠
+        </h1>
+        <div className="text-center">
+          <br />
+          <p className="text-lg">
+            Harga Normal <span className="line-through">Rp.249.000</span>
+          </p>
+          <p className="text-lg">Harga Promo</p>
+        </div>
+        <div className="text-center text-5xl font-bold " ref={promoRef}>
+          <h1 className="text-redto">
+            Rp {Math.floor(currentValue).toLocaleString("id-ID")}
+          </h1>
+				<p className="text-lg text-center italic"> Bonus 4 kepala + Gratis Ongkir </p>
+        </div>
+        {/* <div className="text-center text-sm">
+          <p className="font-semibold ">
+            Bonus 4 kepala + Gratis Ongkir 
+          </p>
+        </div> */}
+        <br />
+        <div className="w-auto h-8 bg-gray-300 rounded overflow-hidden m-2">
+          <p className="bg-redto text-white text-sm px-3 flex items-center h-full w-1/3">
+            Sisa Promo: 1
+          </p>
+        </div>
+
+        {/* <BonusTF
+          valueStack={[
+            {
+              item: "✅ Kamera CCTV Bohlam E27",
+              value: 249000,
+            },
+            {
+              item: "🎁 Bonus Fitting Adapter E27",
+              value: 15000,
+            },
+            {
+              item: "📘 Buku Panduan Instalasi",
+              value: 25000,
+            },
+            {
+              item: "💸 Potongan Ongkir",
+              value: 15000,
+            },
+            {
+              item: "🚀 Pengiriman Prioritas",
+              value: 30000,
+            },
+            {
+              item: "🛡️ Garansi 7 Hari Ganti Baru",
+              value: 40000,
+            },
+          ]}
+          hargaPromo={hargaJual}
+        />
+
+        <h1 className="text-2xl font-bold text-center">
+          ⚠ Promo 50% Hanya Untuk 3 Orang Pemesanan Pertama ⚠
+        </h1>
+        <div className="w-auto h-8 bg-gray-300 rounded overflow-hidden m-2">
+          <p className="bg-redto text-white text-sm px-3 flex items-center h-full w-1/3">
+            Sisa Promo: 1
+          </p>
+        </div> */}
+      </div>
+
+      {/* FORM TITLE + ARROW */}
+      <br />
+      <h1 className="text-center font-bold text-xl p-2">
+        Isi Data Sekarang Sebelum Promonya Habis — Siapa Cepat Dia Dapat!
+      </h1>
+
+      <br />
+      <div className="text-center space-x-6 text-redto text-5xl animate-bounce">
+        <FontAwesomeIcon icon={faArrowDown} />
+        <FontAwesomeIcon icon={faArrowDown} />
+        <FontAwesomeIcon icon={faArrowDown} />
+      </div>
+
+      {/* FORM FUNNEL */}
+      <div id="form">
+        <Funnel
+          pixel={pixelId}
+          product={produkBaru}
+          price={promoPrice}
+          namaProduct={namaProduct}
+        />
+      </div>
+      {/* 
+      <div>
+        <PaymentComparison
+          bonusItem="Bonus Fitting"
+          cashback="Potongan ongkir 15RB"
+          priorityShipping="Proritas Paket"
+          digitalGuide="Buku panduan"
+          guarantee="Garanasi 7Hari"
+          codFee="Biaya COD"
+        />
+      </div> */}
+
+      {/* FAQ */}
+      <div>
+        <Faqs faqs={faqs} />
+      </div>
+
+      {/* FOOTER IMAGE */}
+      <br />
+      <div className="grid justify-center items-center">
+        {footerImages.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt={`footer-${i}`}
+            width="640"
+            height="360"
+            loading="lazy"
+            decoding="async"
+          />
+        ))}
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default Footer;
