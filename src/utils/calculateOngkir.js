2073d203c7f43cbe8d670f0bb924d724127f6ebd
Bama <bamaamamiya@gmail.com>
2026-03-26 03:24:20 +0700

    update

--- src/utils/calculateOngkir.js
+++ src/utils/calculateOngkir.js
@@ -1,18 +1,32 @@
 // utils/calculateOngkir.js
 const shippingMap = {
-  "jakarta": 20000,
+  jakarta: 20000,
   "dki jakarta": 20000,
-  "jkt": 20000,
+  jkt: 20000,
+
   "jawa barat": 20000,
-  "jabar": 20000,
+  jabar: 20000,
+
   "jawa tengah": 20000,
-  "jateng": 20000,
-  "di yogyakarta": 23000,
-  "yogyakarta": 23000,
-  "jogja": 23000,
+  jateng: 20000,
+
   "jawa timur": 25000,
-  "jatim": 25000,
-  "bali": 35000,
+  jatim: 25000,
+
+  "di yogyakarta": 23000,
+  yogyakarta: 23000,
+  jogja: 23000,
+  diy: 23000,
+
+  bali: 35000,
+  banten: 20000, // default seperti Jabar
+  "sumatera utara": 30000,
+  "sumatera barat": 30000,
+  "sumatera selatan": 30000,
+  "sulawesi selatan": 30000,
+  "sulawesi utara": 30000,
+  "kalimantan selatan": 30000,
+  papua: 40000,
 };
 
 // Normalize input: lowercase, remove spaces & dots
--- src/utils/data/provinceDictionary.js
+++ src/utils/data/provinceDictionary.js
@@ -1,28 +1,72 @@
 export const provinceDictionary = {
-  jatim: "Jawa Timur",
-  jateng: "Jawa Tengah",
-  jabar: "Jawa Barat",
-  banten: "Banten",
-  bali: "Bali",
+  // Jakarta
   jkt: "Jakarta",
+  "dki jakarta": "Jakarta",
+  "dki-jkt": "Jakarta",
+  "dkij": "Jakarta",
   jktut: "Jakarta Utara",
   jktbr: "Jakarta Barat",
   jkttr: "Jakarta Timur",
   jktsl: "Jakarta Selatan",
+  jakarta: "Jakarta",
 
-  // ✅ Yogyakarta additions
+  // Jawa Barat
+  jabar: "Jawa Barat",
+  jawabarat: "Jawa Barat",
+  "jawa barat": "Jawa Barat",
+  "jawa-barat": "Jawa Barat",
+
+  // Jawa Tengah
+  jateng: "Jawa Tengah",
+  jawatengah: "Jawa Tengah",
+  "jawa tengah": "Jawa Tengah",
+  "jawa-tengah": "Jawa Tengah",
+
+  // Jawa Timur
+  jatim: "Jawa Timur",
+  jawatimur: "Jawa Timur",
+  "jawa timur": "Jawa Timur",
+  "jawa-timur": "Jawa Timur",
+
+  // DI Yogyakarta
   diy: "DI Yogyakarta",
   jogja: "DI Yogyakarta",
   yogyakarta: "DI Yogyakarta",
+  "di yogyakarta": "DI Yogyakarta",
+  "di-yogyakarta": "DI Yogyakarta",
+  jogjakarta: "DI Yogyakarta",
 
+  // Banten
+  banten: "Banten",
+
+  // Bali
+  bali: "Bali",
+
+  // Sumatera
   sumut: "Sumatera Utara",
+  "sumatera utara": "Sumatera Utara",
   sumbar: "Sumatera Barat",
+  "sumatera barat": "Sumatera Barat",
   sumsel: "Sumatera Selatan",
+  "sumatera selatan": "Sumatera Selatan",
+  sumeks: "Sumatera Selatan", // typo umum
+
+  // Sulawesi
   sulsel: "Sulawesi Selatan",
   sulut: "Sulawesi Utara",
+  sulteng: "Sulawesi Tengah",
+  sultra: "Sulawesi Tenggara",
+  sulbar: "Sulawesi Barat",
+
+  // Kalimantan
   kalsel: "Kalimantan Selatan",
   kaltim: "Kalimantan Timur",
   kalteng: "Kalimantan Tengah",
   kalbar: "Kalimantan Barat",
+  kalut: "Kalimantan Utara",
+
+  // Papua
   papua: "Papua",
+  "papua barat": "Papua Barat",
+  papbar: "Papua Barat",
 };
